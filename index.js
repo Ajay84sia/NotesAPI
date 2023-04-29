e47566a1cfc057e9df25bc42e5f2a2caa3417dc0
Ajay84sia <ajay9679339@gmail.com>
2023-04-29 13:49:28 +0530

    submit

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ require("dotenv").config()
 
 app.use(cors())
 app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).send("Basic API Endpoint")
+})
+
 app.use("/users", userRouter)
 
 app.use(auth)
